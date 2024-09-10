8c8cd7f279fafa3bac59019c5c25d3637041b08f
2°F <jonathan.g.oliveira6@aluno.senai.br>
2024-09-10 10:10:48 -0300

    Novos arquivos de outros assuntos de Carlito

--- estrutura_sequencial/atividades/Atv_Avaliativa/Qt_01.js
+++ estrutura_sequencial/atividades/Atv_Avaliativa/Qt_01.js
@@ -1,13 +1,49 @@
 //Elabore um programa que realize a leitura de um número e, caso o número for par ele exibe o dobro do número e se for ímpar, o triplo. A leitura continua até que o usúario informe 0. Inicialmente, uma mensagem alertando sobre a execução do programa é apresentada.
 
-let numero 
+//continue = faz um novo teste lógico 
+//break = finaliza a estrutura de repetição
 
-numero = parseInt(prompt("Digite um número: "))
+//let numero = 1
 
-if(numero % 2 == 0){
-    document.write(`O número é par que é ${numero} e dobrado ele fica ${numero*2}. <br>`)
-}else{
-    document.write(`O número é ímpar que é ${numero} e triplicado ele fica ${numero*3}.`)
-}
+//1º Maneira 1
+//while(numero != 0){
+//    alert("Entrou no laço")
+//    numero = parseInt(prompt("Digite 0 pra sair"))
+//}
+
+//2ª Maneira - continue - break
+//for(let i = 1;i<=10; i++){
+//    if(i === 5 || i === 8){
+//        continue
+//    }
+//    
+//    if(i === 7){
+//       break
+//   }
+//    console.log("i,:" +i)
+//}
 
 
+alert("Digite 0 para sair do programa")
+let numero
+
+do{
+    numero = parseInt(prompt("Digite um número"))
+    debbuger
+    if(numero ===0 || isNaN(numero)){
+        //confirmar a saída
+        let sair = prompt("Confirmar saída")
+        if(sair){
+            break
+        }else{
+            continue
+        }
+    }
+    //continuar a lógica par(dobro) e impar(triplo)
+    if(numero%2 === 0){
+alert(`O dobro do ${numero} é: ${numero*2}.`)
+    }else{
+        alert(`O triplo do ${numero} é: ${numero*3}.`)
+    }
+    alert("Digite 0 para sair do programa")
+}while (true)
--- estrutura_sequencial/atividades/Atv_Avaliativa/Qt_02.js
+++ estrutura_sequencial/atividades/Atv_Avaliativa/Qt_02.js
@@ -1,17 +1,37 @@
 //Elabore um programa que faz a leitura de contas que devem ser pagas por um usuário. As contas são ixibidas e no final da listagem o número de contas(contador) e a soma dos valores (acumulador) são destacados.
 
-let contador, acumuladora = 0
-let numero, valorf = 0
+//Variáveis contadoras - acumuladoras = iniciadas com o valor 0  
+//1º variavel++, variavel-- 
+//2º variavel = varialvel + acumular 
 
-    contador = parseInt(prompt("Digite o número de contas que você tem que pagar: "))
+let nomeConta, valorConta
+let qtdConta = 0, valorTotal = 0 
+let i =1
 
-do{
-    acumuladora++
-    numero = parseInt(prompt("Digite o valor da conta: "))
-    valorf = valorf + numero
+
+
+while(i !== 0){
+
+    do{
+        valorConta = parseInt(prompt(`Digite o valor da conta ${nomeConta}.`))
+        nomeConta = prompt("Qual conta irá pagar")
+        if(nomeConta === ''){
+            alert("A conta precisa de uma descrição, DIGITE MISERÁ!")
+        }
     
-}while (acumuladora < contador); 
+    }while(nomeConta === '');
 
-document.write(`Você irá pagar ${contador} contas e o valor total será de: ${valorf}.`)
+    do{
+        valorConta = parseInt(prompt(`Digite o valor da conta ${nomeConta}: `))
+        if(valorConta === 0 || isNaN(valorConta)){
+            alert("O valorda conta precisa ser números")
+        }
+    }while(valorConta === 0 || isNaN(valorConta));
 
+    qtdConta++
+    valorTotal = valorTotal + valorConta
 
+    document.write(`<h4>${nomeConta} \t\t ------ \t\t ${valorConta} </h4>`)
+    i = parseInt(prompt("Digite 1 para continuar cadastrado e 0 para sair"))
+}
+document.write(`<h3>Quantidade de contas ${qtdConta} ------ ${valorTotal}</h3>`)
